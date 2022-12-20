22ec0ecea8e618ab6c8fca27c2f8854850e2e6f2
mtheuslipe <matheusfelipe.mf454@gmail.com>
2022-12-19 23:08:27 -0300

    prática de condicionais em ranges

--- src/main/kotlin/rage-conditional.kt
+++ src/main/kotlin/rage-conditional.kt
@@ -3,6 +3,7 @@ fun main(){
     if(x in 1..5){
         println("x está no intervalo de 1 até 5")
     }
+    println()
     if(x !in 5..10){
         println("x não está no intervalo de 6 até 10")
     }
