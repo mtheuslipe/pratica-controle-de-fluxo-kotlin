f5795dfc247d6bb30f108979452861d5d93c90c3
mtheuslipe <matheusfelipe.mf454@gmail.com>
2022-12-19 23:02:12 -0300

    prrática para absorver como funcionam os rage e mais loops em kotlin

---
+++ src/main/kotlin/rage.kt
@@ -0,0 +1,22 @@
+fun main(){
+    for(i in 0..3){
+        print(i)
+    }
+    println()
+    for(i in 0 until 3){
+        print(i)
+    }
+    println()
+    for(i in 2..8 step 2){
+        print(i)
+    }
+    println()
+    for(i in 3 downTo 0){
+        print(i)
+    }
+    println()
+    for(i in 10 downTo 0 step 2){
+        print(i)
+    }
+    println()
+}
